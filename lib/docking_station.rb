417c06fad10d244462fd04c90e397400c2bd2b3e
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-02 09:21:28 +0100

    REFACTOR: Privates methods full? adn empty? functionable.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,13 +6,23 @@ class DockingStation
   end
 
   def release_bike
-    raise "There are no docked bikes." if @bikes.empty?
+    raise "There are no docked bikes." if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "Docking station full." if @bikes.size >= 20
+    raise "Docking station full." if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.size >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
