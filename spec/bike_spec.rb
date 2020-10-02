f683907933996f121b3ce914240b01e6305cb9f8
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-02 11:11:03 +0100

    RED: no broken? or is_broken methods for bikes.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,11 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to(:working?) }
+
+  describe '#is_broken' do
+    it 'reports bike as broken' do
+      subject.is_broken
+      expect(subject.broken?).to eq(true)
+    end
+  end
 end
