226707593cc63bd01586f648456f126803ec727d
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-02 10:31:11 +0100

    RED: Docking stations don't have variable capacity.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,18 @@ require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
 
+  describe '#initialize' do
+    it 'has default capacity of 20' do
+      expect(subject.capacity).to eq(DockingStation::DEFAULT_CAPACITY)
+    end
+
+    it 'has a variable capacity' do
+      large_station = DockingStation.new(50)
+      50.times { large_station.dock(Bike.new) }
+      expect { large_station.dock(Bike.new) }.to raise_error "Docking station full."
+    end
+  end
+
   describe '#release_bike' do
     it 'releases bikes that work' do
       bike = Bike.new
